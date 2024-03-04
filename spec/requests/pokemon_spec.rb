10fc18f6bc23826fd38947c71e76f02a4e5b9fe4
inoue-keiichi <inouejai@gmail.com>
2024-03-05 01:43:24 +0900

    追加コンテンツの場合の pokemon#showの spec 追加

--- spec/requests/pokemon_spec.rb
+++ spec/requests/pokemon_spec.rb
@@ -112,6 +112,109 @@ RSpec.describe 'Pokemon', type: :request do
       )
     end
 
+    context 'get pokemon data whose version group is additional contents' do
+      let(:id){ 1024 } # テラパゴス
+
+      it do
+        get api_pokemon_path(id), params: {region: 'paldea'}
+        expect(response).to have_http_status(:success)
+        expect(response.parsed_body).to match(
+          {
+            'damage_from' => {'bug' => 1, 'dark' => 1, 'dragon' => 1, 'electric' => 1, 'fairy' => 1, 'fighting' => 2, 'fire' => 1, 'flying' => 1, 'ghost' => 0, 'grass' => 1, 'ground' => 1, 'ice' => 1, 'normal' => 1, 'poison' => 1, 'psychic' => 1, 'rock' => 1, 'steel' => 1, 'water' => 1},
+            'flavor_text' => nil,
+            'id' => 1024,
+            'moves' => [
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'ちょうおんぱ'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'のしかかり'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'とっしん'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'アイアンヘッド'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'ストーンエッジ'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'ステルスロック'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'ヘビースラム'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'アシストパワー'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'ワイルドボルト'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'ヒートスタンプ'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'マジカルシャイン'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'ボディプレス'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'メテオビーム'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'あなをほる'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'こおりのつむじ'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'すてみタックル'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'ほえる'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'かえんほうしゃ'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'なみのり'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'れいとうビーム'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'はかいこうせん'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'ソーラービーム'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => '１０まんボルト'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'かみなり'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'じしん'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'どくどく'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'ねむる'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'いわなだれ'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'みがわり'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'まもる'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'がまん'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'ねごと'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'あまごい'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'にほんばれ'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'かみくだく'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'からげんき'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'おおぞらほう'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'めいそう'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'みずのはどう'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'じゅうりょく'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'ジャイロボール'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'フレアドライブ'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'はどうだん'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'あくのはどう'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'むしのさざめき'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'りゅうのはどう'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'パワージェム'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'エナジーボール'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'だいちのちから'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'ギガインパクト'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'しねんのずつき'},
+              {'level_learned_at' => 0, 'move_learn_method' => 'machine', 'name' => 'ラスターカノン'},
+              {'level_learned_at' => 1, 'move_learn_method' => 'level-up', 'name' => 'からにこもる'},
+              {'level_learned_at' => 1, 'move_learn_method' => 'level-up', 'name' => 'こうそくスピン'},
+              {'level_learned_at' => 1, 'move_learn_method' => 'level-up', 'name' => 'トライアタック'},
+              {'level_learned_at' => 10, 'move_learn_method' => 'level-up', 'name' => 'ねんぱつ'},
+              {'level_learned_at' => 20, 'move_learn_method' => 'level-up', 'name' => 'ずつき'},
+              {'level_learned_at' => 30, 'move_learn_method' => 'level-up', 'name' => 'まもる'},
+              {'level_learned_at' => 40, 'move_learn_method' => 'level-up', 'name' => 'だいちのちから'},
+              {'level_learned_at' => 50, 'move_learn_method' => 'level-up', 'name' => 'ヘビースラム'},
+              {'level_learned_at' => 60, 'move_learn_method' => 'level-up', 'name' => 'てんそくせん'},
+              {'level_learned_at' => 70, 'move_learn_method' => 'level-up', 'name' => 'すてみタックル'},
+              {'level_learned_at' => 80, 'move_learn_method' => 'level-up', 'name' => 'ロックカット'},
+              {'level_learned_at' => 90, 'move_learn_method' => 'level-up', 'name' => 'ジャイロボール'}
+            ],
+            'name' => 'テラパゴス',
+            'sprites' => {
+              'back_default' => nil,
+              'back_female' => nil,
+              'back_shiny' => nil,
+              'back_shiny_female' => nil,
+              'front_default' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1024.png',
+              'front_female' => nil,
+              'front_shiny' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1024.png',
+              'front_shiny_female' => nil
+            },
+            'status' => {
+              'attack' => {'base_stat' => 65, 'effort' => 0},
+              'defense' => {'base_stat' => 85, 'effort' => 1},
+              'hp' => {'base_stat' => 90, 'effort' => 0},
+              'special_attack' => {'base_stat' => 65, 'effort' => 0},
+              'special_defense' => {'base_stat' => 85, 'effort' => 0},
+              'speed' => {'base_stat' => 60, 'effort' => 0}
+            },
+            'types' => [{'name' => 'normal', 'slot' => 1}],
+            'version_group' => 'scarlet-violet'
+          }
+        )
+      end
+    end
+
     context 'get pokemon data which has moves with same names' do
       let(:id) { 3 } # フシギバナ
 

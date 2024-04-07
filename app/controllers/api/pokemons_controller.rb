f4a6aa9f2730e3d7af165d72a79ed0f6c01e2e26
inoue-keiichi <inouejai@gmail.com>
2024-04-07 15:23:06 +0900

    input_formを共通化

--- app/controllers/api/pokemons_controller.rb
+++ app/controllers/api/pokemons_controller.rb
@@ -7,7 +7,7 @@ class Api::PokemonsController < ApplicationController
   end
 
   def show
-    input = Api::Pokemons::Show::InputForm.new(id: params[:id].to_i, version_group: params[:version_group])
+    input = Api::Pokemons::Shared::InputForm.new(id: params[:id].to_i, version_group: params[:version_group])
     return render(status: :bad_request, json: {errors: input.errors}) unless input.valid?
 
     pokemon_profile = PokemonProfile.new(id: input.id, version_group: input.version_group)
@@ -15,7 +15,7 @@ class Api::PokemonsController < ApplicationController
   end
 
   def forms
-    input = Api::Pokemons::Forms::InputForm.new(id: params[:id].to_i, version_group: params[:version_group])
+    input = Api::Pokemons::Shared::InputForm.new(id: params[:id].to_i, version_group: params[:version_group])
     output = FetchForms.new.execute(id: input.id, version_group: input.version_group)
     render json: Pokemon::Forms::Serializer.new.serialize(output).to_json
   end

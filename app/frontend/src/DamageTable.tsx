90763dcecfe7074040797588617ae4e64208ce76
inoue-keiichi <inouejai@gmail.com>
2024-04-22 01:04:20 +0900

    ダメージ表にフィルターを追加する

--- app/frontend/src/DamageTable.tsx
+++ app/frontend/src/DamageTable.tsx
@@ -1,26 +1,25 @@
-import { GridBlock, NumericTable, NumericTableHeader } from "@freee_jp/vibes";
+import {
+  ColumnBase,
+  FormControl,
+  GridBlock,
+  HStack,
+  MultiComboBox,
+  MultiComboBoxOption,
+  NumericTable,
+} from "@freee_jp/vibes";
+import { useState } from "react";
 
-const createTypeDamageMap = () => {
-  return new Map([
-    ["normal", { value: "", order: 0 }],
-    ["fire", { value: "", order: 1 }],
-    ["water", { value: "", order: 2 }],
-    ["electric", { value: "", order: 3 }],
-    ["grass", { value: "", order: 4 }],
-    ["ice", { value: "", order: 5 }],
-    ["fighting", { value: "", order: 6 }],
-    ["poison", { value: "", order: 7 }],
-    ["ground", { value: "", order: 8 }],
-    ["flying", { value: "", order: 9 }],
-    ["psychic", { value: "", order: 10 }],
-    ["bug", { value: "", order: 11 }],
-    ["rock", { value: "", order: 12 }],
-    ["ghost", { value: "", order: 13 }],
-    ["dragon", { value: "", order: 14 }],
-    ["dark", { value: "", order: 15 }],
-    ["steel", { value: "", order: 16 }],
-    ["fairy", { value: "", order: 17 }],
-  ]);
+type DamageType = {
+  id: number;
+  label: string;
+  value: string;
+  damage: Damage;
+};
+
+const createTypeToDamageValue = (damageTypes: DamageType[]) => {
+  return new Map(
+    damageTypes.map((type) => [type.value, { value: "", order: type.id }])
+  );
 };
 
 type Damage = {
@@ -32,57 +31,106 @@ type Damage = {
   no_to: string[];
 };
 
-const aggregateDamageTo = (damage: Damage) => {
-  const result = createTypeDamageMap();
-  for (const type of damage.double_to) {
-    const damage = result.get(type);
-    if (damage) {
-      damage.value = "⚪︎";
-    }
-  }
-  for (const type of damage.half_to) {
-    const damage = result.get(type);
-    if (damage) {
-      damage.value = "△";
-    }
-  }
-  for (const type of damage.no_to) {
-    const damage = result.get(type);
-    if (damage) {
-      damage.value = "×";
-    }
-  }
-
-  const damages = [...result.values()];
-  return damages
-    .sort((a, b) => a.order - b.order)
-    .map((damage) => ({
-      value: damage.value,
-    }));
-};
-
-const createRows = () => {
-  return Object.values(TYPE_MAP)
-    .sort((a, b) => a.order - b.order)
-    .map((type) => {
-      const cells = [];
-      cells.push({ value: type.name });
-      cells.push(...aggregateDamageTo(type.damage));
-      return { cells };
-    });
-};
-
 export function DamageTable() {
-  const headers: NumericTableHeader[] = TYPE_HEADERS.sort(
-    (a, b) => a.order - b.order
-  ).map((type) => ({
-    value: type.name,
-  }));
-  headers.unshift({ value: "" });
+  const [damageToTypes, setDamageToTypes] = useState<MultiComboBoxOption[]>([]);
+  const [damageFromTypes, setDamageFromTypes] = useState<MultiComboBoxOption[]>(
+    []
+  );
 
-  const rows = createRows();
+  const options = DAMAGE_TYPES.map((type) => ({
+    ...type,
+    keywords: [type.label],
+  }));
+
+  const damageToTypeLabels = damageToTypes.map((type) => type.label);
+  const damageTos =
+    damageToTypeLabels.length === 0
+      ? DAMAGE_TYPES
+      : DAMAGE_TYPES.filter((row) => damageToTypeLabels.includes(row.label));
+
+  const damageFromTypeLabels = damageFromTypes.map((type) => type.label);
+  const damageFroms =
+    damageFromTypeLabels.length === 0
+      ? DAMAGE_TYPES
+      : DAMAGE_TYPES.filter((row) => damageFromTypeLabels.includes(row.label));
+
+  const headers = createHeaders(damageFroms);
+  const rows = damageTos.map((damageTo) => createRow(damageTo, damageFroms));
+
+  function createHeaders(damageFroms: DamageType[]) {
+    const headers = damageFroms.map((type) => ({
+      value: type.label,
+    }));
+    headers.unshift({ value: "" });
+    return headers;
+  }
+
+  function createRow(damageTo: DamageType, damageFroms: DamageType[]) {
+    const typeToDamageValue = createTypeToDamageValue(damageFroms);
+    for (const type of damageTo.damage.double_to) {
+      const damage = typeToDamageValue.get(type);
+      if (damage) {
+        damage.value = "⚪︎";
+      }
+    }
+    for (const type of damageTo.damage.half_to) {
+      const damage = typeToDamageValue.get(type);
+      if (damage) {
+        damage.value = "△";
+      }
+    }
+    for (const type of damageTo.damage.no_to) {
+      const damage = typeToDamageValue.get(type);
+      if (damage) {
+        damage.value = "×";
+      }
+    }
+
+    const cells = [...typeToDamageValue.values()]
+      .sort((a, b) => a.order - b.order)
+      .map((damage) => ({
+        value: damage.value,
+      }));
+    cells.unshift({ value: damageTo.label });
+    return {
+      cells,
+    };
+  }
+
   return (
     <>
+      <ColumnBase>
+        <HStack justifyContent={"center"}>
+          <div style={{ textAlign: "left" }}>
+            <FormControl label="与えるダメージ" fieldId="damageToMultiComboBox">
+              <MultiComboBox
+                id="damageToMultiComboBox"
+                values={damageToTypes}
+                options={options}
+                onChange={(e) => {
+                  setDamageToTypes(e);
+                }}
+              />
+            </FormControl>
+          </div>
+          <div style={{ textAlign: "left" }}>
+            <FormControl
+              label="与えられるダメージ"
+              fieldId="damageFromMultiComboBox"
+            >
+              <MultiComboBox
+                id="damageFromMultiComboBox"
+                values={damageFromTypes}
+                options={options}
+                onChange={(e) => {
+                  setDamageFromTypes(e);
+                }}
+              />
+            </FormControl>
+          </div>
+        </HStack>
+      </ColumnBase>
+
       <h2>与えられるダメージ</h2>
       <GridBlock size={"half"}>
         <div
@@ -95,37 +143,23 @@ export function DamageTable() {
           <h2>与えるダメージ</h2>
         </div>
         <div style={{ display: "inline-block" }}>
-          <NumericTable rowHeaderCount={1} headers={headers} rows={rows} />
+          <NumericTable
+            fixedRowHeader
+            rowHeaderCount={1}
+            headers={headers}
+            rows={rows}
+          />
         </div>
       </GridBlock>
     </>
   );
 }
 
-const TYPE_HEADERS = [
-  { name: "ノーマル", order: 0 },
-  { name: "ほのお", order: 1 },
-  { name: "みず", order: 2 },
-  { name: "でんき", order: 3 },
-  { name: "くさ", order: 4 },
-  { name: "こおり", order: 5 },
-  { name: "かくとう", order: 6 },
-  { name: "どく", order: 7 },
-  { name: "じめん", order: 8 },
-  { name: "ひこう", order: 9 },
-  { name: "エスパー", order: 10 },
-  { name: "むし", order: 11 },
-  { name: "いわ", order: 12 },
-  { name: "ゴースト", order: 13 },
-  { name: "ドラゴン", order: 14 },
-  { name: "あく", order: 15 },
-  { name: "はがね", order: 16 },
-  { name: "フェアリー", order: 17 },
-];
-
-const TYPE_MAP = {
-  normal: {
-    name: "ノーマル",
+const DAMAGE_TYPES = [
+  {
+    id: 1,
+    label: "ノーマル",
+    value: "normal",
     damage: {
       double_from: ["fighting"],
       double_to: [],
@@ -134,10 +168,76 @@ const TYPE_MAP = {
       no_from: ["ghost"],
       no_to: ["ghost"],
     },
-    order: 0,
   },
-  fighting: {
-    name: "かくとう",
+  {
+    id: 2,
+    label: "ほのお",
+    value: "fire",
+    damage: {
+      double_from: ["ground", "rock", "water"],
+      double_to: ["bug", "steel", "grass", "ice"],
+      half_from: ["bug", "steel", "fire", "grass", "ice", "fairy"],
+      half_to: ["rock", "fire", "water", "dragon"],
+      no_from: [],
+      no_to: [],
+    },
+  },
+  {
+    id: 3,
+    label: "みず",
+    value: "water",
+    damage: {
+      double_from: ["grass", "electric"],
+      double_to: ["ground", "rock", "fire"],
+      half_from: ["steel", "fire", "water", "ice"],
+      half_to: ["water", "grass", "dragon"],
+      no_from: [],
+      no_to: [],
+    },
+  },
+  {
+    id: 4,
+    label: "でんき",
+    value: "electric",
+    damage: {
+      double_from: ["ground"],
+      double_to: ["flying", "water"],
+      half_from: ["flying", "steel", "electric"],
+      half_to: ["grass", "electric", "dragon"],
+      no_from: [],
+      no_to: ["ground"],
+    },
+  },
+  {
+    id: 5,
+    label: "くさ",
+    value: "grass",
+    damage: {
+      double_from: ["flying", "poison", "bug", "fire", "ice"],
+      double_to: ["ground", "rock", "water"],
+      half_from: ["ground", "water", "grass", "electric"],
+      half_to: ["flying", "poison", "bug", "steel", "fire", "grass", "dragon"],
+      no_from: [],
+      no_to: [],
+    },
+  },
+  {
+    id: 6,
+    label: "こおり",
+    value: "ice",
+    damage: {
+      double_from: ["fighting", "rock", "steel", "fire"],
+      double_to: ["flying", "ground", "grass", "dragon"],
+      half_from: ["ice"],
+      half_to: ["steel", "fire", "water", "ice"],
+      no_from: [],
+      no_to: [],
+    },
+  },
+  {
+    id: 7,
+    label: "かくとう",
+    value: "fighting",
     damage: {
       double_from: ["flying", "psychic", "fairy"],
       double_to: ["normal", "rock", "steel", "ice", "dark"],
@@ -146,22 +246,11 @@ const TYPE_MAP = {
       no_from: [],
       no_to: ["ghost"],
     },
-    order: 6,
   },
-  flying: {
-    name: "ひこう",
-    damage: {
-      double_from: ["rock", "electric", "ice"],
-      double_to: ["fighting", "bug", "grass"],
-      half_from: ["fighting", "bug", "grass"],
-      half_to: ["rock", "steel", "electric"],
-      no_from: ["ground"],
-      no_to: [],
-    },
-    order: 9,
-  },
-  poison: {
-    name: "どく",
+  {
+    id: 8,
+    label: "どく",
+    value: "poison",
     damage: {
       double_from: ["ground", "psychic"],
       double_to: ["grass", "fairy"],
@@ -170,10 +259,11 @@ const TYPE_MAP = {
       no_from: [],
       no_to: ["steel"],
     },
-    order: 7,
   },
-  ground: {
-    name: "じめん",
+  {
+    id: 9,
+    label: "じめん",
+    value: "ground",
     damage: {
       double_from: ["water", "grass", "ice"],
       double_to: ["poison", "rock", "steel", "fire", "electric"],
@@ -182,22 +272,37 @@ const TYPE_MAP = {
       no_from: ["electric"],
       no_to: ["flying"],
     },
-    order: 8,
   },
-  rock: {
-    name: "いわ",
+  {
+    id: 10,
+    label: "ひこう",
+    value: "flying",
     damage: {
-      double_from: ["fighting", "ground", "steel", "water", "grass"],
-      double_to: ["flying", "bug", "fire", "ice"],
-      half_from: ["normal", "flying", "poison", "fire"],
-      half_to: ["fighting", "ground", "steel"],
-      no_from: [],
+      double_from: ["rock", "electric", "ice"],
+      double_to: ["fighting", "bug", "grass"],
+      half_from: ["fighting", "bug", "grass"],
+      half_to: ["rock", "steel", "electric"],
+      no_from: ["ground"],
       no_to: [],
     },
-    order: 12,
   },
-  bug: {
-    name: "むし",
+  {
+    id: 11,
+    label: "エスパー",
+    value: "psychic",
+    damage: {
+      double_from: ["bug", "ghost", "dark"],
+      double_to: ["fighting", "poison"],
+      half_from: ["fighting", "psychic"],
+      half_to: ["steel", "psychic"],
+      no_from: [],
+      no_to: ["dark"],
+    },
+  },
+  {
+    id: 12,
+    label: "むし",
+    value: "bug",
     damage: {
       double_from: ["flying", "rock", "fire"],
       double_to: ["grass", "psychic", "dark"],
@@ -214,10 +319,24 @@ const TYPE_MAP = {
       no_from: [],
       no_to: [],
     },
-    order: 11,
   },
-  ghost: {
-    name: "ゴースト",
+  {
+    id: 13,
+    label: "いわ",
+    value: "rock",
+    damage: {
+      double_from: ["fighting", "ground", "steel", "water", "grass"],
+      double_to: ["flying", "bug", "fire", "ice"],
+      half_from: ["normal", "flying", "poison", "fire"],
+      half_to: ["fighting", "ground", "steel"],
+      no_from: [],
+      no_to: [],
+    },
+  },
+  {
+    id: 14,
+    label: "ゴースト",
+    value: "ghost",
     damage: {
       double_from: ["ghost", "dark"],
       double_to: ["ghost", "psychic"],
@@ -226,10 +345,37 @@ const TYPE_MAP = {
       no_from: ["normal", "fighting"],
       no_to: ["normal"],
     },
-    order: 13,
   },
-  steel: {
-    name: "はがね",
+  {
+    id: 15,
+    label: "ドラゴン",
+    value: "dragon",
+    damage: {
+      double_from: ["ice", "dragon", "fairy"],
+      double_to: ["dragon"],
+      half_from: ["fire", "water", "grass", "electric"],
+      half_to: ["steel"],
+      no_from: [],
+      no_to: ["fairy"],
+    },
+  },
+  {
+    id: 16,
+    label: "あく",
+    value: "dark",
+    damage: {
+      double_from: ["fighting", "bug", "fairy"],
+      double_to: ["ghost", "psychic"],
+      half_from: ["ghost", "dark"],
+      half_to: ["fighting", "dark", "fairy"],
+      no_from: ["psychic"],
+      no_to: [],
+    },
+  },
+  {
+    id: 17,
+    label: "はがね",
+    value: "steel",
     damage: {
       double_from: ["fighting", "ground", "fire"],
       double_to: ["rock", "ice", "fairy"],
@@ -249,106 +395,11 @@ const TYPE_MAP = {
       no_from: ["poison"],
       no_to: [],
     },
-    order: 16,
   },
-  fire: {
-    name: "ほのお",
-    damage: {
-      double_from: ["ground", "rock", "water"],
-      double_to: ["bug", "steel", "grass", "ice"],
-      half_from: ["bug", "steel", "fire", "grass", "ice", "fairy"],
-      half_to: ["rock", "fire", "water", "dragon"],
-      no_from: [],
-      no_to: [],
-    },
-    order: 1,
-  },
-  water: {
-    name: "みず",
-    damage: {
-      double_from: ["grass", "electric"],
-      double_to: ["ground", "rock", "fire"],
-      half_from: ["steel", "fire", "water", "ice"],
-      half_to: ["water", "grass", "dragon"],
-      no_from: [],
-      no_to: [],
-    },
-    order: 2,
-  },
-  grass: {
-    name: "くさ",
-    damage: {
-      double_from: ["flying", "poison", "bug", "fire", "ice"],
-      double_to: ["ground", "rock", "water"],
-      half_from: ["ground", "water", "grass", "electric"],
-      half_to: ["flying", "poison", "bug", "steel", "fire", "grass", "dragon"],
-      no_from: [],
-      no_to: [],
-    },
-    order: 4,
-  },
-  electric: {
-    name: "でんき",
-    damage: {
-      double_from: ["ground"],
-      double_to: ["flying", "water"],
-      half_from: ["flying", "steel", "electric"],
-      half_to: ["grass", "electric", "dragon"],
-      no_from: [],
-      no_to: ["ground"],
-    },
-    order: 3,
-  },
-  psychic: {
-    name: "エスパー",
-    damage: {
-      double_from: ["bug", "ghost", "dark"],
-      double_to: ["fighting", "poison"],
-      half_from: ["fighting", "psychic"],
-      half_to: ["steel", "psychic"],
-      no_from: [],
-      no_to: ["dark"],
-    },
-    order: 10,
-  },
-  ice: {
-    name: "こおり",
-    damage: {
-      double_from: ["fighting", "rock", "steel", "fire"],
-      double_to: ["flying", "ground", "grass", "dragon"],
-      half_from: ["ice"],
-      half_to: ["steel", "fire", "water", "ice"],
-      no_from: [],
-      no_to: [],
-    },
-    order: 5,
-  },
-  dragon: {
-    name: "ドラゴン",
-    damage: {
-      double_from: ["ice", "dragon", "fairy"],
-      double_to: ["dragon"],
-      half_from: ["fire", "water", "grass", "electric"],
-      half_to: ["steel"],
-      no_from: [],
-      no_to: ["fairy"],
-    },
-    order: 14,
-  },
-  dark: {
-    name: "あく",
-    damage: {
-      double_from: ["fighting", "bug", "fairy"],
-      double_to: ["ghost", "psychic"],
-      half_from: ["ghost", "dark"],
-      half_to: ["fighting", "dark", "fairy"],
-      no_from: ["psychic"],
-      no_to: [],
-    },
-    order: 15,
-  },
-  fairy: {
-    name: "フェアリー",
+  {
+    id: 18,
+    label: "フェアリー",
+    value: "fairy",
     damage: {
       double_from: ["poison", "steel"],
       double_to: ["fighting", "dragon", "dark"],
@@ -357,8 +408,7 @@ const TYPE_MAP = {
       no_from: ["dragon"],
       no_to: [],
     },
-    order: 17,
   },
-};
+];
 
 export default DamageTable;
--- scripts/clazz/type_list_fetcher.rb
+++ scripts/clazz/type_list_fetcher.rb
@@ -2,38 +2,41 @@
 
 class TypeListFetcher
   TARGET_DIR = 'pokemon_data'
-  TYPES = ['normal', 'fighting', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost', 'steel', 'fire', 'water', 'grass', 'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy'].freeze
+  # 並び順を公式と一緒にしているので変えないこと
+  TYPES = ['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy'].freeze
   # order を付与するもっといい方法があるかも
-  TYPE_ORDERS = [
-    { name: 'normal', order: 0 },
-    { name: 'fire', order: 1 },
-    { name: 'water', order: 2 },
-    { name: 'electric', order: 3 },
-    { name: 'grass', order: 4 },
-    { name: 'ice', order: 5 },
-    { name: 'fighting', order: 6 },
-    { name: 'poison', order: 7 },
-    { name: 'ground', order: 8 },
-    { name: 'flying', order: 9 },
-    { name: 'psychic', order: 10 },
-    { name: 'bug', order: 11 },
-    { name: 'rock', order: 12 },
-    { name: 'ghost', order: 13 },
-    { name: 'dragon', order: 14 },
-    { name: 'dark', order: 15 },
-    { name: 'steel', order: 16 },
-    { name: 'fairy', order: 17 }
-  ].freeze
+  # TYPE_ORDERS = [
+  #   {id:1, name: 'normal', order: 0 },
+  #   {id:2, name: 'fire', order: 1 },
+  #   {id:3, name: 'water', order: 2 },
+  #   {id:4, name: 'electric', order: 3 },
+  #   {id:5, name: 'grass', order: 4 },
+  #   {id:6, name: 'ice', order: 5 },
+  #   {id:7, name: 'fighting', order: 6 },
+  #   {id:8, name: 'poison', order: 7 },
+  #   {id:9,name: 'ground', order: 8 },
+  #   {id:10, name: 'flying', order: 9 },
+  #   {id:11, name: 'psychic', order: 10 },
+  #   {id:12, name: 'bug', order: 11 },
+  #   {id:13, name: 'rock', order: 12 },
+  #   {id:14, name: 'ghost', order: 13 },
+  #   {id:15, name: 'dragon', order: 14 },
+  #   {id:16, name: 'dark', order: 15 },
+  #   {id:17, name: 'steel', order: 16 },
+  #   {id:18, name: 'fairy', order: 17 }
+  # ].freeze
 
   def self.execute
     FileUtils.mkdir_p(TARGET_DIR)
 
     puts 'fetching type list ...'
     File.open("#{TARGET_DIR}/type_list.json", 'w') do |file|
-      type_hash = TYPES.index_with do |type|
+      result = TYPES.map.with_index do |type, i|
         damage_relations = PokeApi.get(type: type).damage_relations
         {
-          name: I18n.t("type.#{type}"),
+          id: i + 1,
+          label: I18n.t("type.#{type}"),
+          value: type,
           damage: {
             double_from: damage_relations.double_damage_from.map(&:name),
             double_to: damage_relations.double_damage_to.map(&:name),
@@ -42,10 +45,9 @@ class TypeListFetcher
             no_from: damage_relations.no_damage_from.map(&:name),
             no_to: damage_relations.no_damage_to.map(&:name),
           },
-          order: TYPE_ORDERS.find { |type_order| type_order[:name] == type }[:order]
-        }
+        }.stringify_keys
       end
-    JSON.dump(type_hash.stringify_keys, file)
+    JSON.dump(result, file)
     end
     puts 'finish!!'
   end
